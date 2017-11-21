b0df354c6517942f7f1cb5bdc9fc2c2bc9f8ab1b
SubhaPrince <subhasisgiri01@gmail.com>
2017-11-22 00:01:39 +0530

    Added data in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#================== Topic DATA =================
+Topic.create(name: 'Photography', description: "Photography is the science, art, application and practice of creating durable images by recording light")
+Topic.create(name: 'Space', description: "Space is the boundless three-dimensional extent in which objects and events have relative position and direction")
+#================== END OF Topic SEEDING =================
+
+#================== User DATA =================
+User.create(full_name: "subhasis", age: 27, phone: "9635177341",email: "subhasisgiri01@gmail.com", password: "qwerty")
+User.create(full_name: "Asish", age: 27, phone: "9590213409",email: "asishgiri01@gmail.com", password: "qwerty")
+#================== END OF User DATA =================
