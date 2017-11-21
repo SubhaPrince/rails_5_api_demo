300c50aad06f2fd70e3dd729757bef907f4af077
SubhaPrince <subhasisgiri01@gmail.com>
2017-11-21 23:18:58 +0530

    override create mathod for user to handle more efficiant way

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,42 @@
 class User < ApplicationRecord
-	has_secure_token
-	has_secure_password # it tell to load bcrypt-ruby
+  has_secure_token
+  has_secure_password # it tell to load bcrypt-ruby
+
+  def self.create(params)
+    retry_count = 2
+    begin
+      if params[:email].blank?
+        raise ArgumentError.new("email is a required field")
+      end
+      if params[:password].blank?
+        raise ArgumentError.new("password is a required field")
+      end
+
+      user = User.new
+      user[:email] = params[:email]
+      user[:password] = params[:password]
+      user[:full_name] = params[:full_name]
+      user[:age] = params[:age]
+      dup = user.duplicate
+      if dup.present?
+        return dup
+      end
+      if user.save
+        return user
+      else
+        puts "DEBUG>> error while saving User: #{user.errors.inspect}"
+        return nil
+      end
+    rescue ActiveRecord::RecordNotUnique => e
+      puts "DEBUG>> caught record not unique exception while making User: #{e.message}"
+      retry_count -= 1
+      retry if retry_count > 0
+      puts e.backtrace.join("\n\t")
+      return nil
+    end
+  end
+
+  def duplicate
+    return User.where("email = ?", self.email).take
+  end
 end
