d36ba59c291e8d76773dc09ec1106fb2d60bf991
SubhaPrince <subhasisgiri01@gmail.com>
2017-11-21 10:24:35 +0530

    user model and migration added

---
+++ db/migrate/20171121044428_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password_digest
+      t.string :full_name
+      t.integer :age
+      t.string :token
+      t.timestamps
+    end
+    add_index :users, :token, unique: true
+  end
+end
