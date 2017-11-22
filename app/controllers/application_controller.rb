5fda90536ae849bc59bd550ea4bb0dc6d9dd173b
SubhaPrince <subhasisgiri01@gmail.com>
2017-11-22 22:32:44 +0530

    added authorization methods to verify the perticular user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::API
+  #to varify the authenticate user
+  def authenticate_user!
+    authenticate_token || render_unauthorized("Access denied")
+  end
+
+  # Returns a current logged in user
+  def current_user
+    @current_user ||= authenticate_token
+  end
+
+  protected
+  # returns a message for unauthorized user
+  def render_unauthorized(message)
+    errors = { errors: [ { detail: message } ] }
+    render json: errors, status: :unauthorized
+  end
+  private
+  # authenticate_with_http_token takes the token provided in the header of the http request 
+  # makes it available in the token block variable
+  def authenticate_token
+    authenticate_with_http_token do |token, options|
+      User.find_by(token: token)
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,6 @@ Topic.create(name: 'Space', description: "Space is the boundless three-dimension
 #================== END OF Topic SEEDING =================
 
 #================== User DATA =================
-User.create(full_name: "subhasis", age: 27, phone: "9635177341",email: "subhasisgiri01@gmail.com", password: "qwerty")
-User.create(full_name: "Asish", age: 27, phone: "9590213409",email: "asishgiri01@gmail.com", password: "qwerty")
+User.create(full_name: "subhasis", age: 27, email: "subhasisgiri01@gmail.com", password: "qwerty")
+User.create(full_name: "Asish", age: 27, email: "asishgiri01@gmail.com", password: "qwerty")
 #================== END OF User DATA =================
