bd6baf36c8746be76f54cb8fdf76b4c72ea5ac3a
SubhaPrince <subhasisgiri01@gmail.com>
2017-11-23 01:21:55 +0530

    version wise api structure enhancement

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # only allow the json format request by specifying the format in the constraints option
-  scope :format => true, :constraints => { :format => 'json' } do
-    post   "/login"       => "sessions#create"
-    delete "/logout"      => "sessions#destroy"
+  namespace :api, defaults: { format: :json } do
+    # only allow the json format request by specifying the format in the constraints option
+    scope module: :v1, :format => true, :constraints => { :format => 'json' } do
+      post   "/login"       => "sessions#create"
+      delete "/logout"      => "sessions#destroy"
+    end
   end
 end
