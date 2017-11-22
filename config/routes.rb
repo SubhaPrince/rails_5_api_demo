664ba0edc130f11c65d8627b7451d3fba8edca6a
SubhaPrince <subhasisgiri01@gmail.com>
2017-11-22 23:19:52 +0530

    Updated routes for accessing the seassion actions and spefied constrains to accept only json data

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # only allow the json format request by specifying the format in the constraints option
+  scope :format => true, :constraints => { :format => 'json' } do
+    post   "/login"       => "sessions#create"
+    delete "/logout"      => "sessions#destroy"
+  end
 end
